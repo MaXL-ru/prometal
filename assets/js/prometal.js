9e096bebdfd52c623df77e89ac811da61522dfd2
MaXL <m.lukashchuk@gmail.com>
2016-09-19 22:10:29 +1000

    Add basic markup

---
+++ assets/js/prometal.js
@@ -0,0 +1,22 @@
+/**
+ * 19.09.2016
+ * File: prometal
+ * Encoding: UTF-8
+ * Author: MaXL
+ * */
+
+$(function () {
+  $('.watch-trailer').fancybox(
+    {
+      maxWidth   : '100%',
+      maxHeight  : '100%',
+      fitToView  : false,
+      //width      : '70%',
+      //height     : '70%',
+      autoSize   : false,
+      closeClick : false,
+      openEffect : 'none',
+      closeEffect: 'none'
+    }
+  );
+});
